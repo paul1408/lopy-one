1b52b598662f5318ec0567e0cebaf47e69eb4d75
Paul Newmann <s.paul1408@gmail.com>
2017-11-14 10:42:03 +0200

    acceleration reading OK

--- acc.py
+++ acc.py
@@ -1,10 +1,21 @@
+#acc from pysense
 from LIS2HH12 import LIS2HH12
 from pysense import Pysense
 
+class Sensor_acc:
 
-def init_acc():
-    py = Pysense()
-    accel = LIS2HH12()
+    def __init__(self):
+        self.py = Pysense()
+        self.sensor = LIS2HH12()
 
-def getacc():
-    return LIS2HH12.acceleration()
+    def getacc(self):
+        a = self.sensor.acceleration()
+        return a
+
+    def getpitch(self):
+        p = self.sensor.pitch()
+        return p
+
+    def getroll(roll):
+        r = self.sensor.roll()
+        return r
--- main.py
+++ main.py
@@ -3,16 +3,15 @@ from network import LoRa
 import socket
 import machine
 import time
-import acc #created module
-
+from acc import Sensor_acc
 #setup LoRa network and socket
 #lora = LoRa(mode=LoRa.LORA)
 #s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 
-#setup acc
-acc.init_acc()
+accel = Sensor_acc()
 
 #read from UART
-uart.write(acc.getacc)
-time.sleep_ms(500)
-
+while True:
+    acc_all = accel.getacc()
+    print('Acc: {}'.format(acc_all))
+    time.sleep_ms(500)
